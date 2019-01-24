e71ec716bc5e4c0629f7adee4b1ec80cf615ce2a
Pavel Silin <psilin@topcon.com>
2019-01-24 16:18:04 +0300

    hw3

---
+++ hw3_bench/fast.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"sync"
+
+	//json "encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
+)
+
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+type User struct {
+	Browsers []string
+	Email    string
+	Name     string
+}
+
+func easyjson248bee40DecodeEasyjt(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "email":
+			out.Email = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson248bee40EncodeEasyjt(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"email\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"name\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Name))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson248bee40EncodeEasyjt(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson248bee40EncodeEasyjt(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson248bee40DecodeEasyjt(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson248bee40DecodeEasyjt(l, v)
+}
+
+// вам надо написать более быструю оптимальную этой функции
+func FastSearch(out io.Writer) {
+	var dataPool = sync.Pool{
+		New: func() interface{} {
+			return make([]byte, 0, 500)
+		},
+	}
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(file)
+	cnt := 0
+
+	seenBrowsers := make([]string, 0, 115)
+
+	fmt.Fprintln(out, "found users:")
+	u := &User{}
+	for {
+		cnt++
+		str := dataPool.Get().([]byte)
+		str, err = reader.ReadBytes('\n')
+		u.UnmarshalJSON(str)
+		dataPool.Put(str)
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range u.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+				}
+			}
+
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+				}
+			}
+		}
+
+		if isAndroid && isMSIE {
+			u.Email = strings.Replace(u.Email, "@", " [at] ", -1)
+			fmt.Fprintf(out, "[%d] %s <%s>\n", cnt-1, u.Name, u.Email)
+		}
+
+		// fmt.Printf("%d: %s %s %d\n", cnt, u.Name, u.Email, len(u.Browsers))
+		if err == io.EOF {
+			break
+		}
+	}
+	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
+}
