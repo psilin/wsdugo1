cd232562ba1b1a384939d115d97b163da4c1fdf5
psilin <silinp@yandex.ru>
2020-04-13 21:10:04 +0200

    refactoring + generation in one big template (turned to be a bad idea yet it works)

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -10,6 +10,181 @@ import (
 	"os"
 	"reflect"
 	"strings"
+	"text/template"
+)
+
+var (
+	muxTpl = template.Must(template.New("muxTpl").Parse(`
+func (h {{(index . 0).RcvName}}) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path { {{range $key := .}}
+	case "{{($key).URL}}":
+		h.handler{{($key).FuncName}}(w, r){{end}}
+	default:
+		rsp := make(map[string]interface{})
+		rsp["error"] = "unknown method"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusNotFound)
+	}
+}
+`))
+
+	methodTpl = template.Must(template.New("methodTpl").Parse(`
+func (h {{.Func.RcvName}}) handler{{.Func.FuncName}}(w http.ResponseWriter, r *http.Request) {
+	// post {{if .Func.MethodPost}}
+	if r.Method != http.MethodPost {
+		rsp := make(map[string]interface{})
+		rsp["error"] = "bad method"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusNotAcceptable)
+		return
+	}{{end}}
+
+	// auth {{if .Func.Auth}}
+	if r.Header.Get("x-auth") != "100500" {
+		rsp := make(map[string]interface{})
+		rsp["error"] = "unauthorized"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}			
+		http.Error(w, string(data), http.StatusForbidden)
+		return
+	}{{end}}
+
+	// param check
+	var in {{.Func.ParamStructName}}
+	queryName := ""
+	{{range $f := .Fields}}queryName = strings.ToLower("{{($f).FName}}")
+	{{if ($f).FParamNameFlag}}queryName = "{{($f).FParamName}}"{{end}}
+	{{if eq ($f).FType "int"}}val{{($f).FName}}, err{{($f).FName}} := strconv.Atoi(r.FormValue(queryName))
+	if err{{($f).FName}} != nil {
+		rsp := make(map[string]interface{})
+		rsp["error"] = queryName +" must be int"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusBadRequest)
+		return
+	}
+	in.{{($f).FName}} = val{{($f).FName}}
+	{{else}}in.{{($f).FName}} = r.FormValue(queryName){{end}}
+	{{if ($f).FRequiredFlag}}
+	if in.{{($f).FName}} == "" {
+		rsp := make(map[string]interface{})
+		rsp["error"] = queryName +" must me not empty"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusBadRequest)
+		return
+	}{{end}}
+	{{if ($f).FMinFlag}}min{{($f).FName}} := {{($f).FMin}}
+	{{if eq ($f).FType "int"}}
+	if in.{{($f).FName}} < min{{($f).FName}} {
+	{{else}}
+	if len(in.{{($f).FName}}) < min{{($f).FName}} {
+	{{end}}
+		rsp := make(map[string]interface{})
+		rsp["error"] = queryName +" {{if ne ($f).FType "int"}}len {{end}}must be >= "+ "{{($f).FMin}}"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusBadRequest)
+		return
+	}{{end}}
+	{{if ($f).FMaxFlag}}max{{($f).FName}} := {{($f).FMax}} 
+	{{if eq ($f).FType "int"}}
+	if in.{{($f).FName}} > max{{($f).FName}} {
+	{{else}}
+	if len(in.{{($f).FName}}) > max{{($f).FName}} {
+	{{end}}
+		rsp := make(map[string]interface{})
+		rsp["error"] = queryName +" {{if ne ($f).FType "int"}}len {{end}}must be <= "+ "{{($f).FMax}}"
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusBadRequest)
+		return
+	}{{end}}
+	{{if ($f).FDefaultFlag}}
+	if in.{{($f).FName}} == "" {
+		in.{{($f).FName}} = "{{($f).FDefault}}"
+	}{{end}}
+	{{if ($f).FEnumFlag}}
+	found{{($f).FName}} := false
+	enum{{($f).FName}} := "["
+	{{range $ff := ($f).FEnum}}
+	enum{{($f).FName}} = enum{{($f).FName}} + "{{$ff}}" + ", "
+	found{{($f).FName}} = found{{($f).FName}} || ("{{$ff}}" == in.{{($f).FName}})
+	{{end}}
+	enum{{($f).FName}} = strings.TrimSuffix(enum{{($f).FName}}, ", ") + "]"
+	if !found{{($f).FName}} {
+		rsp := make(map[string]interface{})
+		rsp["error"] = queryName + " must be one of " + enum{{($f).FName}} 
+		data, err := json.Marshal(rsp)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		http.Error(w, string(data), http.StatusBadRequest)
+		return
+	}{{end}}
+	{{end}}
+	// call
+	ctx := r.Context()
+	usr, err := h.{{.Func.FuncName}}(ctx, in)
+	if err != nil {
+		if err, ok := err.(ApiError); ok {
+			rsp := make(map[string]interface{})
+			rsp["error"] = err.Err.Error()
+			data, err1 := json.Marshal(rsp)
+			if err1 != nil {
+				log.Fatal(err)
+				return
+			}	
+			http.Error(w, string(data), err.HTTPStatus)
+			return
+		}
+		rsp := make(map[string]interface{})
+		rsp["error"] = err.Error()
+		data, err1 := json.Marshal(rsp)
+		if err1 != nil {
+			log.Fatal(err)
+			return
+		}	
+		http.Error(w, string(data), http.StatusInternalServerError)
+		return
+	}
+
+	rsp := make(map[string]interface{})
+	rsp["error"] = ""
+	rsp["response"] = usr
+	data, err := json.Marshal(rsp)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}	
+	http.Error(w, string(data), http.StatusOK)
+	return
+}
+`))
 )
 
 // GenFunc - what we need to parse for func
@@ -29,12 +204,12 @@ func ParseFuncComment(in, fname, psname, rcvname string) GenFunc {
 
 	trimmed := strings.TrimPrefix(in, "// apigen:api {")
 	trimmed = strings.TrimSuffix(trimmed, "}")
+	trimmed = strings.ReplaceAll(trimmed, " ", "")
+	trimmed = strings.ReplaceAll(trimmed, "\"", "")
 
 	trSl := strings.Split(trimmed, ",")
 
 	for _, s := range trSl {
-		s = strings.ReplaceAll(s, " ", "")
-		s = strings.ReplaceAll(s, "\"", "")
 		//fmt.Printf("    %s - %s\n", s[:strings.Index(s, ":")], s[strings.Index(s, ":")+1:])
 		value := s[strings.Index(s, ":")+1:]
 		switch name := s[:strings.Index(s, ":")]; name {
@@ -89,25 +264,57 @@ func PopulateFuntions(node *ast.File, src string) []GenFunc {
 
 // GenField - what we need to parse for struct's field
 type GenField struct {
-	FName         string
-	FType         string
-	FRequiredFlag bool
-	FEnumFlag     bool
-	FEnum         []string
-	FDefaultFlag  bool
-	FDefault      string
-	FMinflag      bool
-	FMin          string
-	FMaxFlag      bool
-	FMax          string
+	FName          string
+	FType          string
+	FRequiredFlag  bool
+	FEnumFlag      bool
+	FEnum          []string
+	FDefaultFlag   bool
+	FDefault       string
+	FMinFlag       bool
+	FMin           string
+	FMaxFlag       bool
+	FMax           string
+	FParamNameFlag bool
+	FParamName     string
 }
 
 // ParseFieldTag - parses field tag string to create GenField
 func ParseFieldTag(fname, ftype, ftag string) GenField {
 	fmt.Printf("  Field: %s %s %s\n", fname, ftype, ftag)
-	ret := GenField{fname, ftype, false, false, []string{}, false, "", false, "", false, ""}
+	ret := GenField{fname, ftype, false, false, []string{}, false, "", false, "", false, "", false, ""}
 
-	//TODO: fill GenField based on TAG
+	tagSl := strings.Split(ftag, ",")
+	for _, s := range tagSl {
+		if s == "required" {
+			ret.FRequiredFlag = true
+			continue
+		}
+
+		value := s[strings.Index(s, "=")+1:]
+		name := s[:strings.Index(s, "=")]
+		switch name {
+		case "enum":
+			ret.FEnumFlag = true
+			valueSl := strings.Split(value, "|")
+			for _, v := range valueSl {
+				ret.FEnum = append(ret.FEnum, v)
+			}
+		case "default":
+			ret.FDefaultFlag = true
+			ret.FDefault = value
+		case "min":
+			ret.FMinFlag = true
+			ret.FMin = value
+		case "max":
+			ret.FMaxFlag = true
+			ret.FMax = value
+		case "paramname":
+			ret.FParamNameFlag = true
+			ret.FParamName = value
+		}
+	}
+
 	return ret
 }
 
@@ -166,6 +373,12 @@ func PopulateStructs(node *ast.File) map[string][]GenField {
 	return structs
 }
 
+// GenCompleteFunc - function pluss associated parameters
+type GenCompleteFunc struct {
+	Func   GenFunc
+	Fields []GenField
+}
+
 func main() {
 	// read file as a source to serch text in it
 	content, err := ioutil.ReadFile(os.Args[1])
@@ -184,15 +397,27 @@ func main() {
 	// first pass: iterate functions and fill list
 	fmt.Println("FIRST PASS: functions")
 	funcs := PopulateFuntions(node, src)
+	// repack to API
+	apis := make(map[string][]GenFunc)
+	for _, f := range funcs {
+		if _, ok := apis[f.RcvName]; !ok {
+			apis[f.RcvName] = []GenFunc{}
+
+		}
+		apis[f.RcvName] = append(apis[f.RcvName], f)
+	}
 
 	// second pass: iterate structs
 	fmt.Println("SECOND PASS: structs")
 	structs := PopulateStructs(node)
 
 	// result of 2 passes
-	fmt.Printf("FUNCTIONS:\n")
-	for _, f := range funcs {
-		fmt.Printf("  %v\n", f)
+	fmt.Printf("APIS:\n")
+	for s, fS := range apis {
+		fmt.Printf("  API: %s\n", s)
+		for _, f := range fS {
+			fmt.Printf("    FUNC: %v\n", f)
+		}
 	}
 
 	fmt.Printf("STRUCTS:\n")
@@ -205,9 +430,23 @@ func main() {
 
 	// now use all parsed data to generate wrappers
 	fmt.Println("HANDLERS GENERATION")
-	//TODO create handlers based on templates and filled structs
 	out, _ := os.Create(os.Args[2])
 
 	fmt.Fprintln(out, `package `+node.Name.Name)
 	fmt.Fprintln(out) // empty line
+	fmt.Fprintln(out, `import "encoding/json"`)
+	fmt.Fprintln(out, `import "log"`)
+	fmt.Fprintln(out, `import "net/http"`)
+	fmt.Fprintln(out, `import "strconv"`)
+	fmt.Fprintln(out, `import "strings"`)
+	fmt.Fprintln(out) // empty line
+	for _, api := range apis {
+		fmt.Printf("Process API %s\n", api[0].RcvName)
+		muxTpl.Execute(out, api)
+		for _, f := range api {
+			fmt.Printf("  Process method %s\n", f.FuncName)
+			compf := GenCompleteFunc{f, structs[f.ParamStructName]}
+			methodTpl.Execute(out, compf)
+		}
+	}
 }
